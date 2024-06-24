f645488b79710010a512a5b08633af46a62445a3
rogermiranda1000 <rogermirandaperez@hotmail.com>
2024-06-24 16:45:01 +0200

    Fix: install

--- requirements.py
+++ requirements.py
@@ -2,5 +2,5 @@ from javascript import require
 
 require('mineflayer', 'latest')
 require('mineflayer-pathfinder')
-require('node-canvas-webgl') # Prismarine Viewer dependency
-require('prismarine-viewer')
+#require('node-canvas-webgl') # Prismarine Viewer dependency
+#require('prismarine-viewer')
